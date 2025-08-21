690c28cc38e0280603c06ae87998f14beef6af1a
aanesn <anesen.niklas@gmail.com>
2025-08-20 20:07:12 -0400

    header fix

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,7 @@
-<header class="mt-16 flex h-16 items-center justify-between">
-	<h1 class="text-lg font-medium">aanesn</h1>
-	<div class="flex items-center gap-x-2">
-		<a href="https://x.com/aanesn" target="_blank" rel="noreferrer">twitter</a>
-		/
-		<a href="https://github.com/aanesn" target="_blank" rel="noreferrer">github</a>
-	</div>
+<header class="mt-20 flex items-center justify-center gap-x-2">
+	<a href="/">aanesn</a>
+	/
+	<a href="https://x.com/aanesn" target="_blank" rel="noreferrer">twitter</a>
+	/
+	<a href="https://github.com/aanesn" target="_blank" rel="noreferrer">github</a>
 </header>
