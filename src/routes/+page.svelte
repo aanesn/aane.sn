dbe9628c1ea6a8bb8447275abe1c5f90ea21c64d
aanesn <anesen.niklas@gmail.com>
2025-08-20 21:59:58 -0400

    added bio and projects

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<svelte:head>
+	<title>aanesn</title>
+	<meta name="description" content="my personal site" />
+</svelte:head>
+
+<main class="flex flex-col">
+	<section>
+		<h2>$ whoami</h2>
+		<p>
+			currently a cs undergrad at mcgill trying to build something useful. i think ml, hacking
+			and infrastructure is interesting.
+		</p>
+	</section>
+	<section>
+		<h2>$ ls ~/projects</h2>
+		<ul>
+			<li>
+				<a href="https://github.com/aanesn/lambda" target="_blank" rel="noreferrer">
+					lambda
+				</a>
+				<span>a lambda deployment tool</span>
+			</li>
+		</ul>
+	</section>
+</main>
