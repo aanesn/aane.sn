1ac784634c8d21049c7a3f36e853e4881b70a440
aanesn <anesen.niklas@gmail.com>
2025-08-21 12:22:57 -0400

    small fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,14 @@
 
 <main class="flex flex-col">
 	<section>
-		<h2>$ whoami</h2>
+		<h2>whoami</h2>
 		<p>
 			currently a cs undergrad at mcgill trying to build something useful. i think ml, hacking
 			and infrastructure is interesting.
 		</p>
 	</section>
 	<section>
-		<h2>$ ls ~/projects</h2>
+		<h2>projects</h2>
 		<ul>
 			<li>
 				<a href="https://github.com/aanesn/lambda" target="_blank" rel="noreferrer">
